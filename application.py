4d8932e7a9f67e60f1c9a5f2f93c158227782b4c
Yulia <suita98@gmail.com>
2018-07-15 20:20:35 +0300

    Implement login and logout processing

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask, session, render_template, request
-# from flask_session import Session
+from flask import Flask, session, render_template, request, redirect, url_for
+from flask_session import Session
 
 from models import db
 from models import User
@@ -12,23 +12,37 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
+
+
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if "username" not in session:
+        return redirect(url_for('login'))
+    else:
+        return render_template("index.html")
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
         name = request.form['name']
         login = request.form['login']
         email = request.form['email']
         password = request.form['password']
+        # TODO: keep hashed password instead of plain text
+        # TODO: user is unique
         user = User(login=login, password=password, name=name, email=email)
         try:
             db.session.add(user)
@@ -36,9 +50,27 @@ def register():
             return "user saved"
         except Exception as e:
             return "user not saved\n" + str(e)
-    else:
-        return render_template('register.html')
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        login = request.form['login']
+        password = request.form['password']
+        user = User.query.filter_by(login=login).first()
+        if user is not None:
+            if user.password == password:
+                session['username'] = login
+                return redirect(url_for('index'))
+            else:
+                return 'Password doesnt match'
+        else:
+            return "User not found"
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
