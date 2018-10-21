6eafa01b6517f4bbf6340460bf405e36738f9fce
Yulia <suita98@gmail.com>
2018-10-21 12:57:54 +0300

    Implement non-ORM csv data importing

--- import_books.py
+++ import_books.py
@@ -1,26 +1,27 @@
-import csv
+"""This module imports books into books table 
+and should be executed after executing module 
+that creates tables"""
+
 import os
+import csv
 
-from flask import Flask
-from models import db
-from models import Book
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+# Establish connection with database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     f = open("books.csv")
     f.readline()
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
-        book = Book(isbn=isbn, title=title, author=author, year=year)
-        db.session.add(book)
+        db.execute('INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year);', 
+        {'isbn': isbn, 'title': title, 'author': author, 'year': year})
         print(f"Added: {isbn}, {title}, {author}, {year}")
-    db.session.commit()
+    db.commit()
     print('Done')
 
 if __name__ == "__main__":
-    with app.app_context():
-        main()
+    main()
