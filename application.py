7ec63fb0266d7458055815601cf54775bc1e4f76
Yulia <suita98@gmail.com>
2018-10-21 10:35:17 +0300

    Final commit for ORM implementation

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 import os
 
+import requests
+import json
+
 from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import or_
@@ -27,10 +30,12 @@ db.init_app(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if "id" not in session:
+    if not is_logged_in():
         return redirect(url_for('login'))
+    else:
+        user = User.query.get(session['id'])
     if request.method == 'GET':
-        return render_template("index.html")
+        return render_template("index.html", user=user)
     if request.method == 'POST':
         search_string = request.form['search_string']
         books = Book.query.filter(or_(
@@ -39,12 +44,19 @@ def index():
             Book.author.ilike(f"%{search_string}%")
             )).all()
         if not books:
-            return render_template("index.html", message="No books were found")
-        return render_template("search_results.html", books=books)
+            return render_template("index.html", message="No books were found", user=user)
+        return render_template("search_results.html", books=books, user=user)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if is_logged_in():
+        try:
+            messages.append("You are already registered")
+        except NameError:
+            messages = ["You are already registered"]
+        user = User.query.get(session['id'])
+        return render_template('index.html', messages=messages, user=user)
     if request.method == 'GET':
         return render_template('register.html')
     if request.method == 'POST':
@@ -65,6 +77,13 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if is_logged_in():
+        try:
+            messages.append("You are already logged in")
+        except NameError:
+            messages = ["You are already logged in"]
+        user = User.query.get(session['id'])
+        return render_template('index.html', messages=messages, user=user)
     if request.method == 'GET':
         return render_template('login.html')
     if request.method == 'POST':
@@ -83,19 +102,25 @@ def login():
 
 @app.route("/logout", methods=['GET'])
 def logout():
-    session.pop('username', None)
+    session.pop('id', None)
     return redirect(url_for('index'))
 
     
 @app.route("/book/<id>", methods=['GET', 'POST'])
 def book(id):
+    if not is_logged_in():
+        return redirect(url_for('login'))
+    else:
+        user = User.query.get(session['id'])
     errors = []
     book = Book.query.get(id)
     reviews = Review.query.filter_by(book_id=id)
     users_review = Review.query.filter_by(book_id = id).filter_by(user_id = session['id']).first()
 
+    goodreads_info = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "U3PyMNiFCwRX3CE0noLqVg", "isbns": "{}".format(book.isbn)}).json()['books'][0]
+    print(goodreads_info)
     if request.method == 'GET':
-        return render_template('book.html', book=book, reviews=reviews, users_review=users_review)
+        return render_template('book.html', book=book, reviews=reviews, users_review=users_review, goodreads_info=goodreads_info, user=user)
 
     if request.method == 'POST':
         if users_review:
@@ -115,5 +140,30 @@ def book(id):
             db.session.add(review)
             db.session.commit()
             users_review = Review.query.filter_by(book_id = id).filter_by(user_id = session['id']).first()
-            
-        return render_template('book.html', book=book, reviews=reviews, users_review=users_review, errors=errors)
+
+        return render_template('book.html', book=book, reviews=reviews, users_review=users_review, errors=errors, goodreads_info=goodreads_info, user=user)
+
+
+@app.route("/api/<isbn>", methods=['GET'])
+def api(isbn):
+    book = Book.query.filter_by(isbn=isbn).first()
+    # in not book:
+    #     return 
+    reviews = Review.query.filter_by(book_id=book.id).all()
+    review_count = len(reviews)
+    sum_rating = 0
+    for review in reviews:
+        sum_rating += review.rating
+    average_rating = sum_rating / review_count
+    result = {
+        "title": book.title,
+        "author": book.author,
+        "year": book.year,
+        "isbn": book.isbn,
+        "review_count": review_count,
+        "average_score": average_rating
+    }
+    return json.dumps(result)
+
+def is_logged_in():
+    return 'id' in session.keys()
